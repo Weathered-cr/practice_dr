fa248a24e79cd8f703f92e7936235c9eb088aff0
Илья Дорожко <gr424_doise@lab-402-2.tomtit-tomsk.ru>
2024-10-21 11:10:22 +0700

    main

--- bboard/catalog/apps.py
+++ bboard/catalog/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class MainConfig(AppConfig):
     verbose_name = 'Электронная доска объявлений'
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'catalog'

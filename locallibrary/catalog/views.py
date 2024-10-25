1c0440f39bbcc10ddae61b182004ccf6e7c95b2d
Илья Дорожко <gr424_doise@lab-402-2.tomtit-tomsk.ru>
2024-10-25 09:55:00 +0700

    registration

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -87,3 +87,14 @@ class AuthorDetailView(generic.DetailView):
 
         context['books'] = Book.objects.filter(author=self.object)
         return context
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
